b8d77a6a9ba352ceabbc1d03d504dcabc598eefa
ariefahh <74385012+ariefahh@users.noreply.github.com>
2021-11-18 21:02:40 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Animate on Scroll
+AOS.init({
+    duration: 1000,
+})
+
+// Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
